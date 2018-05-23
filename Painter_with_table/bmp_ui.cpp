9ce380acd143928de4e9a31b7f0b6517ca8387d3
SuperSolik <serpetr99@gmail.com>
2018-05-23 14:52:41 +0300

    painter with palette (8 bit)

---
+++ Painter_with_table/bmp_ui.cpp
@@ -0,0 +1,387 @@
+#include "bmp_ui.h"
+#include <QDebug>
+#define PAL_SIZE 256
+#define PADDING2(x) ((4 - (x%4))%4)
+
+BMP_ui::BMP_ui(int x, int y){
+    memset(&b_header, 0,  sizeof(b_header));
+    memset(&b_info, 0,  sizeof(b_info));
+    b_info.biHeight = y;
+    b_info.biWidth = x;
+    b_header.b1 = 'B';
+    b_header.b2 = 'M';
+    b_header.bfSize = sizeof(BMPheader)+
+                      sizeof(BITMAPinfo)+
+                      3*b_info.biWidth*b_info.biHeight+
+                      b_info.biHeight*PADDING(b_info.biWidth);
+    b_header.bfOffBits = 54;
+    b_info.biPlanes = 1;
+    b_info.biSize = sizeof(BITMAPinfo);
+    b_info.biBitCount = 24;
+    pixels = new RGBTriple* [b_info.biHeight];
+    for(int i = 0; i < b_info.biHeight; i++){
+        pixels[i] = new RGBTriple[b_info.biWidth];
+    }
+    created_table = false;
+}
+
+BMP_ui::~BMP_ui(){
+    for(int i = 0; i < b_info.biHeight; i++){
+        delete [] pixels[i];
+    }
+    delete [] pixels;
+    if(created_table){
+        delete [] palette->pixels;
+        delete [] palette;
+    }
+}
+
+void BMP_ui::Clear(){
+    for(int y = 0; y < b_info.biHeight; y++){
+        for(int x = 0; x < b_info.biWidth; x++){
+            pixels[y][x].rgbBlue = 255;
+            pixels[y][x].rgbGreen = 255;
+            pixels[y][x].rgbRed = 255;
+        }
+    }
+}
+
+QImage BMP_ui::DrawImage(){
+    QImage image(b_info.biWidth, b_info.biHeight, QImage::Format_RGB888);
+    QRgb value;
+    for(int y = 0; y < b_info.biHeight; y++){
+        for(int x = 0; x < b_info.biWidth; x++){
+            value = qRgb(pixels[y][x].rgbRed, pixels[y][x].rgbGreen, pixels[y][x].rgbBlue);
+            image.setPixel(x, y, value);
+        }
+    }
+    return image;
+}
+
+void BMP_ui::resizeRaster(int old_y, int new_x, int new_y){
+    b_info.biWidth = new_x;
+    b_info.biHeight = new_y;
+    for(int i = 0; i < old_y; i++){
+        delete [] pixels[i];
+    }
+    delete [] pixels;
+
+    pixels = new RGBTriple* [new_y];
+    for(int i = 0; i < new_y; i++){
+        pixels[i] = new RGBTriple[new_x];
+    }
+}
+
+void BMP_ui::Load(QString filename){
+    int old_y = b_info.biHeight;
+    std::string fname = filename.toStdString();
+    std::ifstream file;
+    file.open(fname, std::ios::in | std::ios::binary);
+    if(!file.is_open()){
+        QMessageBox::information(0, "Error", "File can't be opened, try another!");
+        return;
+    }
+    file.read(reinterpret_cast<char*>(&b_header), sizeof(BMPheader));
+    if(b_header.b1 != 'B' && b_header.b2 != 'M'){
+        QMessageBox::information(0, "Error", "Not a bmp file");
+        return;
+    }
+    file.read(reinterpret_cast<char*>(&b_info), sizeof(BITMAPinfo));
+    if(b_info.biBitCount != 24){
+        QMessageBox::information(0, "Error", "Invalid bitcount");
+        return;
+    }
+    resizeRaster(old_y, b_info.biWidth, b_info.biHeight);
+    file.seekg(b_header.bfOffBits);
+    for(int y = b_info.biHeight - 1; y >= 0; y--){
+        for(int x = 0; x < b_info.biWidth; x++){
+            file.read(reinterpret_cast<char*>(&pixels[y][x]), sizeof(RGBTriple));
+        }
+        if(PADDING(b_info.biWidth) != 0){
+            char temp[PADDING(b_info.biWidth)];
+            file.read(temp, PADDING(b_info.biWidth));
+        }
+    }
+    file.close();
+}
+
+void BMP_ui::Save(QString filename){
+    std::string fname = filename.toStdString();
+    std::ofstream file;
+    file.open(fname, std::ios::out | std::ios::binary);
+    if(!file.is_open()){
+        QMessageBox::information(0, "Error", "File can't be created");
+        return;
+    }
+    BMPheader save_header;
+    BITMAPinfo save_info;
+    mempcpy(&save_header, &b_header, sizeof(BMPheader));
+    mempcpy(&save_info, &b_info, sizeof(BITMAPinfo));
+
+    file.write(reinterpret_cast<char*>(&save_header), sizeof(BMPheader));
+    file.write(reinterpret_cast<char*>(&save_info), sizeof(BITMAPinfo));
+    for(int y = b_info.biHeight - 1; y >= 0; y--){
+        for(int x = 0; x < b_info.biWidth; x++){
+            file.write(reinterpret_cast<char*>(&pixels[y][x]), sizeof(RGBTriple));
+        }
+        if(PADDING(b_info.biWidth) != 0){
+            char temp[PADDING(b_info.biWidth)] = {0};
+            file.write(temp, PADDING(b_info.biWidth));
+        }
+    }
+    file.close();
+}
+
+void BMP_ui::SaveWithTable(QString filename){
+    std::string fname = filename.toStdString();
+    std::ofstream file;
+    file.open(fname, std::ios::out | std::ios::binary);
+    if(!file.is_open()){
+        QMessageBox::information(0, "Error", "File can't be created");
+        return;
+    }
+    BMPheader save_header;
+    BITMAPinfo save_info;
+    mempcpy(&save_header, &b_header, sizeof(BMPheader));
+    mempcpy(&save_info, &b_info, sizeof(BITMAPinfo));
+    unsigned char** raster = new unsigned char*[b_info.biHeight];
+    for(int i = 0; i < b_info.biHeight; i++){
+        raster[i] = new unsigned char[b_info.biWidth];
+    }
+    MakeTable(raster);
+    qDebug() << palette->size;
+    save_header.bfSize = sizeof(BMPheader)+
+                      sizeof(BITMAPinfo)+
+                      //PAL_SIZE*sizeof(RGBQUAD)+
+                      palette->size*sizeof(RGBQUAD)+
+                      b_info.biWidth*b_info.biHeight+
+                      b_info.biHeight*PADDING2(b_info.biWidth);
+    //save_header.bfOffBits = sizeof(BMPheader)+sizeof(BITMAPinfo)+PAL_SIZE*sizeof(RGBQUAD);
+    save_header.bfOffBits = sizeof(BMPheader)+sizeof(BITMAPinfo)+palette->size*sizeof(RGBQUAD);
+    save_info.biBitCount = 8;
+    save_info.biClrUsed = palette->size;
+    file.write(reinterpret_cast<char*>(&save_header), sizeof(BMPheader));
+    file.write(reinterpret_cast<char*>(&save_info), sizeof(BITMAPinfo));
+    //for(int i = 0; i < PAL_SIZE; i++){
+    for(int i = 0; i < palette->size; i++){
+        //qDebug() << palette->pixels[i].rgbRed << palette->pixels[i].rgbGreen << palette->pixels[i].rgbBlue;
+        file.write(reinterpret_cast<char*>(&palette->pixels[i]), sizeof(RGBQUAD));
+    }
+    for(int y = b_info.biHeight - 1; y >= 0; y--){
+        for(int x = 0; x < b_info.biWidth; x++){
+            file.write(reinterpret_cast<char*>(&raster[y][x]), sizeof(unsigned char));
+            if(PADDING2(b_info.biWidth) != 0){
+                char temp[PADDING2(b_info.biWidth)] = {0};
+                file.write(temp, PADDING2(b_info.biWidth));
+            }
+        }
+    }
+    for(int i = 0; i < b_info.biHeight; i++)
+        delete [] raster[i];
+    delete [] raster;
+    file.close();
+}
+
+void BMP_ui::MakeTable(unsigned char** raster){
+    if(created_table){
+        delete [] palette->pixels;
+        delete [] palette;
+    }
+    created_table = true;
+    palette = new Palette;
+    palette->pixels = new RGBQUAD[PAL_SIZE];
+    memset(palette->pixels, 0, sizeof(RGBQUAD)*PAL_SIZE);
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    palette->size = 0;
+    bool found = false;
+    for(int i = 0; i < b_info.biHeight; i++){
+        for(int j = 0; j < b_info.biWidth; j++){
+            found = false;
+            r = pixels[i][j].rgbRed;
+            g = pixels[i][j].rgbGreen;
+            b = pixels[i][j].rgbBlue;
+            for(int n = 0; n < palette->size; n++){
+                if(r == palette->pixels[n].rgbRed && b == palette->pixels[n].rgbBlue && g == palette->pixels[n].rgbGreen){
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                if(palette->size > PAL_SIZE){
+                    QMessageBox::information(0, "Error", "Too much colors");
+                    break;
+                }
+                palette->size++;
+                palette->pixels[palette->size-1].rgbBlue = b;
+                palette->pixels[palette->size-1].rgbGreen = g;
+                palette->pixels[palette->size-1].rgbRed = r;
+            }
+        }
+    }
+    for(int i = 0; i < b_info.biHeight; i++){
+        for(int j = 0; j < b_info.biWidth; j++){
+            for(int n = 0; n < palette->size; n++){
+                if(pixels[i][j].rgbBlue == palette->pixels[n].rgbBlue &&
+                   pixels[i][j].rgbGreen == palette->pixels[n].rgbGreen &&
+                   pixels[i][j].rgbRed == palette->pixels[n].rgbRed){
+                   raster[i][j] = static_cast<unsigned char>(n);
+                }
+            }
+        }
+    }
+}
+
+void BMP_ui::DrawLine(int x1, int y1, int x2, int y2, QColor color, int w){
+    if(x1 > b_info.biWidth - 1 || y1 > b_info.biHeight - 1 || x1 < 0 || y1 < 0){
+        QMessageBox::information(0, "Error", "Wrong coordinates, try again");
+        return;
+    }
+    if(x1 < 0) x1 = 0;
+    if(x2 < 0) x2 = 0;
+    if(y1 < 0) y1 = 0;
+    if(y2 < 0) y2 = 0;
+    if(x1 >= b_info.biWidth) x1 = b_info.biWidth;
+    if(x2 >= b_info.biWidth) x2 = b_info.biWidth;
+    if(y1 >= b_info.biHeight - 1 ) y1 = b_info.biHeight - 1;
+    if(y2 >= b_info.biHeight - 1 ) y2 = b_info.biHeight - 1;
+    int deltaY,  deltaX, sign;
+    deltaY = y2 - y1;
+    deltaX = x1 - x2;
+    if (abs(deltaY) > abs(deltaX)) sign = 1;
+    else sign = -1;
+    int signy, signx;
+    if (deltaY < 0) signy = -1;
+    else signy = 1;
+    if (deltaX < 0) signx = -1;
+    else signx = 1;
+    int f = 0;
+    int x = x1, y = y1;
+    if(sign == -1){
+        while(x != x2 || y != y2){
+            if(w < 1){
+                pixels[y][x].rgbBlue = static_cast<unsigned char>(color.blue());
+                pixels[y][x].rgbGreen = static_cast<unsigned char>(color.green());
+                pixels[y][x].rgbRed = static_cast<unsigned char>(color.red());
+            }else FillCircle(x, y, w, color);
+            f += deltaY*signy;
+            if (f > 0){
+                f -= deltaX*signx;
+                y += signy;
+            }
+            x -= signx;
+        }
+    }
+    else{
+        while(x != x2 || y != y2){
+            if(w < 1){
+                pixels[y][x].rgbBlue = static_cast<unsigned char>(color.blue());
+                pixels[y][x].rgbGreen = static_cast<unsigned char>(color.green());
+                pixels[y][x].rgbRed = static_cast<unsigned char>(color.red());
+            }else FillCircle(x, y, w, color);
+            f += deltaX*signx;
+            if (f > 0) {
+                f -= deltaY*signy;
+                x -= signx;
+            }
+            y += signy;
+        }
+    }
+}
+
+void BMP_ui::FillCircle(int x1, int y1, int r, QColor color){
+    int valuex1 = x1-r;
+    int valuey1 = y1-r;
+    if(valuex1 < 0) valuex1 = 0;
+    if(valuey1 < 0) valuey1 = 0;
+    int valuex2 = x1+r;
+    int valuey2 = y1+r;
+    if(valuex2 > b_info.biWidth) valuex2 = b_info.biWidth;
+    if(valuey2 > b_info.biHeight) valuey2 = b_info.biHeight;
+
+    for(int i  = valuey1; i < valuey2; i++){
+        for(int j  = valuex1; j < valuex2; j++){
+            int f = pow(x1-j,2)+pow(y1-i,2);
+               if ( f<= pow(r,2)){
+                pixels[i][j].rgbBlue = static_cast<unsigned char>(color.blue());
+                pixels[i][j].rgbGreen = static_cast<unsigned char>(color.green());
+                pixels[i][j].rgbRed = static_cast<unsigned char>(color.red());
+            }
+        }
+    }
+}
+
+void BMP_ui::Invert(int x1, int y1, int r){
+    if(x1 > b_info.biWidth - 1 || y1 > b_info.biHeight - 1 || x1 < 0 || y1 < 0){
+        QMessageBox::information(0, "Error", "Wrong coordinates, try again");
+        return;
+    }
+    int valuex1 = x1-r;
+    int valuey1 = y1-r;
+    if(valuex1 < 0) valuex1 = 0;
+    if(valuey1 < 0) valuey1 = 0;
+
+    int valuex2 = x1+r;
+    int valuey2 = y1+r;
+    if(valuex2 > b_info.biWidth) valuex2 = b_info.biWidth;
+    if(valuey2 > b_info.biHeight) valuey2 = b_info.biHeight;
+    for(int i  = valuey1; i < valuey2; i++){
+        for(int j  = valuex1; j < valuex2; j++){
+            int f = pow(x1-j,2)+pow(y1-i,2);
+               if ( f<= pow(r,2)){
+                pixels[i][j].rgbBlue = 255 - pixels[i][j].rgbBlue;
+                pixels[i][j].rgbGreen = 255 - pixels[i][j].rgbGreen;
+                pixels[i][j].rgbRed = 255 - pixels[i][j].rgbRed;
+            }
+        }
+    }
+}
+
+void BMP_ui::Crop(int x1, int y1, int x2, int y2){
+    if(x1 > b_info.biWidth - 1 || y1 > b_info.biHeight - 1 || x1 < 0 || y1 < 0){
+        QMessageBox::information(0, "Error", "Wrong coordinates, try again");
+        return;
+    }
+
+    int begin_x = std::min(x1, x2);
+    int begin_y = std::min(y1, y2);
+    int end_x = std::max(x1, x2);
+    int end_y = std::max(y1, y2);
+
+    if(end_x > b_info.biWidth - 1) end_x = b_info.biWidth - 1;
+    if(end_y > b_info.biHeight - 1) end_y = b_info.biHeight - 1;
+    int temp_w = end_x - begin_x;
+    int temp_h = end_y - begin_y;
+
+    RGBTriple** buffer = new RGBTriple* [temp_h];
+    for(int i = 0; i < temp_h; i++){
+        buffer[i] = new RGBTriple[temp_w];
+    }
+
+    int i = 0, j = 0;
+    for(int y  = begin_y; y < end_y; y++){
+        for(int x  = begin_x; x < end_x; x++){
+            buffer[i][j].rgbBlue = pixels[y][x].rgbBlue;
+            buffer[i][j].rgbGreen = pixels[y][x].rgbGreen;
+            buffer[i][j].rgbRed = pixels[y][x].rgbRed;
+            j++;
+        }
+        j = 0;
+        i++;
+    }
+    int old_y = b_info.biHeight;
+    b_info.biWidth = temp_w;
+    b_info.biHeight = temp_h;
+    resizeRaster(old_y, b_info.biWidth, b_info.biHeight);
+    for(int y  = 0; y < b_info.biHeight; y++){
+        for(int x  = 0; x < b_info.biWidth; x++){
+            pixels[y][x] = buffer[y][x];
+        }
+    }
+    b_info.biWidth = temp_w;
+    b_info.biHeight = temp_h;
+    for(int i = 0; i < temp_h; i++)
+       delete [] buffer[i];
+    delete [] buffer;
+}
